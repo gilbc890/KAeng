586164f149160ed0241df9a4e2b3259ad2bdd428
Byungchan Gil <gilbc890@gmail.com>
2018-11-09 18:50:32 -0500

    company page css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-    
+const menu = document.querySelector('#nav_icon');
+const nav = document.querySelector('.nav_list');
+
+menu.addEventListener('click', () => {
+  if(nav.style.display === "none"){
+  nav.style.display = "block";
+} else {
+  nav.style.display = "none";
+}
 })
